8f44e3aaf948391dd1a861cd42d40bbde1ec3e03
naem1023 <relilau00@gmail.com>
2021-12-03 23:24:09 +0900

    Update data

--- KoBART/run_train.sh
+++ KoBART/run_train.sh
@@ -1,5 +1,6 @@
 python train.py --gradient_clip_val 1.0 \
                 --train_file data/my_train.tsv \
+                --test_file data/my_test.tsv \
                 --max_epochs 5 \
                 --default_root_dir logs \
                 --batch_size 10 \
--- KoGPT/run_train.sh
+++ KoGPT/run_train.sh
@@ -1,1 +1,1 @@
-python train.py  --gradient_clip_val 1.0 --amp_level O3 --precision 16 --max_epochs 5 --train_file data/my_train.tsv --default_root_dir logs  --batch_size 1 --num_workers 4 --max_len 1024 --gpus 1
+python train.py  --gradient_clip_val 1.0 --amp_level O3 --precision 16 --max_epochs 5 --train_file data/my_train.tsv --test_file data/my_test.tsv --default_root_dir logs  --batch_size 1 --num_workers 4 --max_len 1024 --gpus 1
