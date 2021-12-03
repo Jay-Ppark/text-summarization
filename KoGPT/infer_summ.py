0c7418a1a1682ccbd4c8912c26903fc7ec125d98
naem1023 <relilau00@gmail.com>
2021-12-03 04:24:15 +0000

    Fix inference

--- KoGPT/infer_summ.py
+++ KoGPT/infer_summ.py
@@ -3,62 +3,46 @@ import torch
 import torch.nn.functional as F
 from train_ptuning import KoGPTConditionalGeneration
 from utils import generate_next_token
-
-# hparams_file = 'log/hparams.yaml'
-# with open(hparams_file) as f:
-#     hparams = yaml.load(f)
-
-# inf = KoGPTConditionalGeneration.load_from_checkpoint('./log/KoGPT2_summary-last.ckpt', hparams=hparams)
-inf = KoGPTConditionalGeneration.load_from_checkpoint('./logs/model_chp/epoch=01-val_loss=2.282.ckpt')
-
-tokenizer = inf.tokenizer
-SUMMARY = '<unused1>'
-PTUNING = '<unused2>'
-EOS = '</s>'
-
+from transformers import AutoTokenizer, AutoModelForCausalLM 
 import json
 from tqdm.auto import tqdm
 
-with open('../text_summary_dataset_1125/test_summary.json', encoding='UTF-8') as file:
+with open('./data/test_summary.json', encoding='UTF-8') as file:
     test_dataset = json.load(file)
 
+tokenizer = AutoTokenizer.from_pretrained(
+  'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
+  bos_token='[BOS]', eos_token='[EOS]', unk_token='[UNK]', pad_token='[PAD]', mask_token='[MASK]'
+)
+model = AutoModelForCausalLM.from_pretrained(
+  'kakaobrain/kogpt', revision='KoGPT6B-ryan1.5b-float16',  # or float32 version: revision=KoGPT6B-ryan1.5b
+  pad_token_id=tokenizer.eos_token_id,
+  torch_dtype='auto', low_cpu_mem_usage=True
+).to(device='cuda', non_blocking=True)
+_ = model.eval()
 
 my_summaries = []
 error_index = []
 cnt = 0
+
 for paragraph in tqdm(test_dataset):
-    if cnt > 5:
+    if cnt > 20:
         break
     cnt += 1
     original = paragraph['original']
+
     original = original.replace('.', '.\n')
+    original = f'원본: {original}\n요약: '
 
-    text = original.replace('\n', '')
-    input_tokens = tokenizer.encode(PTUNING)* 10 + tokenizer.encode(text) + tokenizer.encode(SUMMARY)
-    input_tensor = torch.tensor(input_tokens).unsqueeze(0)
+    prompt = original
+    with torch.no_grad():
+        tokens = tokenizer.encode(prompt, return_tensors='pt').to(device='cuda', non_blocking=True)
+        gen_tokens = model.generate(tokens, do_sample=True, temperature=0.8, max_length=1000, length_penalty=0.8, repetition_penalty=1.2)
+        summary = tokenizer.batch_decode(gen_tokens)[0]
 
-    eos_id = tokenizer.encode(EOS)[0]
-
-    while True:
-        pred = inf.model(input_tensor)
-        next_token = generate_next_token(pred.logits, temperature=1.0, top_p=0.8)
-
-        if next_token.item() == eos_id:
-            break
-        else:
-            input_tensor = torch.cat([input_tensor, next_token.unsqueeze(0)],1)
-
-    # tokenizer.decode(input_tensor[0])
-
-    summary = tokenizer.decode(input_tensor[0]).split('<unused1>')[-1].strip()
-
+    # summary = summary.split('요약: ')[-1]
     paragraph['summary'] = summary
     my_summaries.append(paragraph)
-
-print('error index')
-print(len(error_index))
-for i in error_index:
-    print(i)
 
 with open('my_summary_sol.json', 'w', encoding="UTF-8") as file:
     json.dump(my_summaries, file, indent='\t', ensure_ascii=False)
--- KoGPT/run_train.sh
+++ KoGPT/run_train.sh
@@ -1,1 +1,1 @@
-python train.py  --gradient_clip_val 1.0 --max_epochs 5 --train_file data/my_train.tsv --default_root_dir logs  --batch_size 2 --num_workers 4 --max_len 1024 --gpus 2
+python train.py  --gradient_clip_val 1.0 --amp_level O3 --precision 16 --max_epochs 5 --train_file data/my_train.tsv --default_root_dir logs  --batch_size 1 --num_workers 4 --max_len 1024 --gpus 1
